b65822c2e99be37ba88a8581cb4632bc49cc59c3
ThomasRomeu <losromeu@hotmail.com>
2023-05-20 11:11:26 -0300

    completo el DetailActivity.kt

--- app/src/main/java/com/example/primerparcial_thomasromeu/DetailActivity.kt
+++ app/src/main/java/com/example/primerparcial_thomasromeu/DetailActivity.kt
@@ -1,11 +1,37 @@
 package com.example.primerparcial_thomasromeu
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var textViewDetail: TextView
+    private lateinit var imageView: ImageView
+    private lateinit var textViewEdad: TextView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        textViewDetail = findViewById(R.id.textViewDetail)
+        textViewEdad = findViewById(R.id.textViewEdad)
+        imageView = findViewById(R.id.imageViewDetail)
+
+        val bundle = intent.extras
+        val nombre = bundle?.getString("nombre", "")
+        val url = bundle?.getString("url") ?: ""
+        val edad = bundle?.getInt("edad", 0)
+
+        textViewDetail.text = nombre
+        textViewEdad.text = edad.toString() + " " + "Años"
+
+        Glide.with(applicationContext)
+            .load(url)
+            .into(imageView)
     }
 }
